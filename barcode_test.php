334d4a8d3042098ceea329bcae3b7cc5485f9382
amazingproducer <amazingproducer@gmail.com>
2020-05-08 07:06:44 +0000

    partial success - ready for plugin-specific endpoint

--- barcode_test.php
+++ barcode_test.php
@@ -6,11 +6,17 @@ echo 'getting results from '.$upc_api_url;
 */
 $upc_api_response = file_get_contents($upc_api_url);
 
-echo $upc_api_response;
 /*
+echo $upc_api_response;
+
 echo 'parsing results';
 */
-$upc_search_result = json_decode($upc_api_response);
-echo $upc_search_result;
-echo 'done';
+$upc_search_result = json_decode($upc_api_response, true);
+//var_dump($upc_search_result);
+foreach($upc_search_result["results"][0]["result"] as $key => $value) {
+  echo $key . " => " . $value . "<br>";
+}
+echo $upc_search_result["results"][0]["result"]["product_name"];
+echo "\r\n";
+echo "done";
 ?>
